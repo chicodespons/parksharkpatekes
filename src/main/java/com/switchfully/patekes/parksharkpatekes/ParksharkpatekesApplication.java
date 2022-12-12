4a8a4d6065e00f0a720d0c542047d3e9b42754a3
chicodespons <pieterdedycker@gmail.com>
2022-12-12 12:03:08 +0100

    first commit

---
+++ src/main/java/com/switchfully/patekes/parksharkpatekes/ParksharkpatekesApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.patekes.parksharkpatekes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParksharkpatekesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParksharkpatekesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/switchfully/patekes/parksharkpatekes/ParksharkpatekesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.patekes.parksharkpatekes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParksharkpatekesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
