9617198333348cb6b642ca87a550b083609e2727
zehSrafe <kratossupermicros@gmail.com>
2022-12-14 10:21:21 +0100

    Correct merge?

--- src/main/java/com/switchfully/patekes/parksharkpatekes/controller/ControllerExceptionHandler.java
+++ src/main/java/com/switchfully/patekes/parksharkpatekes/controller/ControllerExceptionHandler.java
@@ -4,16 +4,25 @@ import com.switchfully.patekes.parksharkpatekes.exceptions.LicencePlateException
 import com.switchfully.patekes.parksharkpatekes.exceptions.MemberException;
 import com.switchfully.patekes.parksharkpatekes.exceptions.ParkingAllocationException;
 import com.switchfully.patekes.parksharkpatekes.exceptions.ParkingLotException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @ControllerAdvice
-public class ControllerExceptionHandler {
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(MemberException.class)
     protected void memberException(MemberException ex, HttpServletResponse response) throws IOException {
         response.sendError(BAD_REQUEST.value(), ex.getMessage());
@@ -32,5 +41,18 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ParkingAllocationException.class)
     protected void parkingAllocationException(ParkingAllocationException ex, HttpServletResponse response) throws IOException {
         response.sendError(BAD_REQUEST.value(), ex.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
     }
 }
