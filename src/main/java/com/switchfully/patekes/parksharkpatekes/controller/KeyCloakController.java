e0f4996c444abdacc4441984ef8b7e86c9b2c1c7
William Multani <william.multani@gmail.com>
2022-12-15 11:09:42 +0100

    Story ML1: basic implementation

--- src/main/java/com/switchfully/patekes/parksharkpatekes/controller/KeyCloakController.java
+++ src/main/java/com/switchfully/patekes/parksharkpatekes/controller/KeyCloakController.java
@@ -1,9 +1,13 @@
 package com.switchfully.patekes.parksharkpatekes.controller;
 
 import com.switchfully.patekes.parksharkpatekes.dto.NewMemberDto;
+import com.switchfully.patekes.parksharkpatekes.dto.UpdateMembershipLevelDto;
 import com.switchfully.patekes.parksharkpatekes.exceptions.KeyCloakCantMakeUserException;
+import com.switchfully.patekes.parksharkpatekes.exceptions.MemberException;
 import com.switchfully.patekes.parksharkpatekes.service.KeyCloakService;
 import com.switchfully.patekes.parksharkpatekes.service.MemberService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,8 +24,14 @@ public class KeyCloakController {
 
     @RequestMapping(method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
     public void createUser(@RequestBody NewMemberDto newMemberDto) throws KeyCloakCantMakeUserException {
-
         keyCloakService.addUser(newMemberDto);
         memberService.addUser(newMemberDto);
+    }
+
+    @PutMapping(path = "/membershiplevel/{id}", consumes = "application/json")
+    @PreAuthorize("hasAnyAuthority('MEMBER')")
+    public void updateMembershipLevel(@RequestBody UpdateMembershipLevelDto updateMembershipLevelDto, @PathVariable Long id, Authentication authentication) throws MemberException {
+        memberService.updateMembershipLevel(updateMembershipLevelDto, id, authentication);
+
     }
 }
--- src/main/java/com/switchfully/patekes/parksharkpatekes/service/MemberService.java
+++ src/main/java/com/switchfully/patekes/parksharkpatekes/service/MemberService.java
@@ -1,13 +1,18 @@
 package com.switchfully.patekes.parksharkpatekes.service;
 
 import com.switchfully.patekes.parksharkpatekes.dto.NewMemberDto;
+import com.switchfully.patekes.parksharkpatekes.dto.UpdateMembershipLevelDto;
+import com.switchfully.patekes.parksharkpatekes.exceptions.MemberException;
 import com.switchfully.patekes.parksharkpatekes.mapper.MemberMapper;
 import com.switchfully.patekes.parksharkpatekes.model.*;
 import com.switchfully.patekes.parksharkpatekes.repository.AddressRepository;
 import com.switchfully.patekes.parksharkpatekes.repository.LicensePlateRepository;
 import com.switchfully.patekes.parksharkpatekes.repository.MemberRepository;
 import com.switchfully.patekes.parksharkpatekes.repository.PostalCodeRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 import static org.apache.coyote.http11.Constants.a;
 
@@ -32,7 +37,7 @@ public class MemberService {
 
     public void addUser(NewMemberDto newMemberDto) {
         // alle elementen die gesaved moeten worden in volgorde voor dat de eigenlijke memeber kan gesaved worden
-        Member member =  memberMapper.CreateMemberfromMemberDto(newMemberDto);
+        Member member = memberMapper.CreateMemberfromMemberDto(newMemberDto);
         LicensePlate licensePlate = member.getLicensePlate();
         // save methodes
         licensePlateRepository.save(licensePlate);
@@ -54,5 +59,41 @@ public class MemberService {
         PostalCode tempPC = checkPostalCode(address.getPostalCode());
         address.setPostalCode(tempPC);
         return addressRepository.save(address);
+    }
+
+    private Member checkMember(Long memberId, Authentication authentication) throws MemberException {
+        Optional<Member> memberFromDb = memberRepository.findById(memberId);
+        if (memberFromDb.isEmpty()) {
+            throw new MemberException("Could not find specified member.");
+        }
+
+//        if (memberFromDb.get().getKeycloakId() == authentication.getName()) {
+//            throw new MemberException("Not authorized");
+//        }
+        else return memberFromDb.get();
+    }
+
+    private MembershipLvl checkMemberShipLevel(UpdateMembershipLevelDto updateMembershipLevelDto) throws MemberException {
+        if (updateMembershipLevelDto == null) {
+            throw new MemberException("Wrong input given: please select a valid Membershiplevel (bronze, silver or gold).");
+        }
+        else if (updateMembershipLevelDto.membershipLvl().equalsIgnoreCase("bronze")) {
+            return MembershipLvl.BRONZE;
+        }
+        else if (updateMembershipLevelDto.membershipLvl().equalsIgnoreCase("silver")) {
+            return MembershipLvl.SILVER;
+        }
+        else if (updateMembershipLevelDto.membershipLvl().equalsIgnoreCase("gold")) {
+            return MembershipLvl.GOLD;
+        }
+        else {
+            throw new MemberException("Wrong input given: please select a valid Membershiplevel (bronze, silver or gold).");
+        }
+    }
+
+    public void updateMembershipLevel(UpdateMembershipLevelDto updateMembershipLevelDto, Long id, Authentication authentication) throws MemberException {
+        Member memberToUpdate = checkMember(id, authentication);
+        memberToUpdate.setMembershipLvl(checkMemberShipLevel(updateMembershipLevelDto));
+        memberRepository.save(memberToUpdate);
     }
 }
